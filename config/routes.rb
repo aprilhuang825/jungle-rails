4aa2107ee438496bc7e53570897a0c7c4e8692b9
Ziyu Huang <aprilhuang825@gmail.com>
2022-07-08 05:50:41 +0000

    Add routes for new sign up

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  get '/signup' => 'persons#new'
+  post '/persons' => 'persons#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
