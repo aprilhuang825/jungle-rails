fd5ae91da4af2747700a77cc385000dc3fa5d767
Ziyu Huang <aprilhuang825@gmail.com>
2022-07-09 16:57:10 +0000

    add create action for persons controller

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -3,5 +3,25 @@ class PersonsController < ApplicationController
   end
 
   def create  
+    person = Person.new(person_params)
+
+    if person.save
+      session[:person_id] = @person.id
+      redirect_to :root, notice: 'User created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
